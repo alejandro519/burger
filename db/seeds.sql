7568b10bdb44a5190e0b34c70857af15ea530951
Real Ghost <alejandro519@gmail.com>
2019-10-09 22:48:34 -0700

    Created schema and seeds for the MySQL database

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+/* Seeds for SQL table. We haven't discussed this type of file yet */
+USE burgers_db;
+
+/* Insert 3 Rows into your new table */
+INSERT INTO burgers (burger_name)
+VALUES ("Sandy Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Breakfast Sausuge Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Meat and Potatoes Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Not a Real Burger, Burger");
